384c59401571c776188d4807dbcc3bbfaa4e7c2c
XjoeX910605 <joexz910605@gmail.com>
2022-05-23 20:50:28 +0800

    Added index.html to draw highchart based on flask data

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template ,jsonify
 import pymysql
 
-app = Flask(__name__)
+db = pymysql.connect(host="localhost", user="test123", password="test123", db="aiotdb")
+cursor = db.cursor()
+
+app = Flask(__name__, template_folder='template')
+
 
 @app.route("/")
-def test():
-	return "Hello World!"
+def index():
+	sql = "SELECT * FROM `sensors`"
+
+	cursor.execute(sql)
+	data = cursor.fetchall()
+
+	return render_template('index.html',data=data)
 
 if __name__ == "__main__":
 	app.run(debug=True)
